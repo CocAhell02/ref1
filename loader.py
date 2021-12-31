07747dbc1034eae8f3931e511c0b1f5169b4ba41
CocAhell02 <90522351+CocAhell02@users.noreply.github.com>
2021-12-31 03:15:32 +0200

    Add files via upload

---
+++ loader.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+#CЛИТО В ТЕЛЕГРАМ КАНАЛЕ  @END_SOFTWARE
+bot = Bot('5011741366:AAG9POPpLacC_7Szu4WFUwEiPE82OmKaefE', parse_mode='html')
+
+#CЛИТО В ТЕЛЕГРАМ КАНАЛЕ  @END_SOFTWARE
+dp = Dispatcher(bot, storage=MemoryStorage())
+#CЛИТО В ТЕЛЕГРАМ КАНАЛЕ  @END_SOFTWARE
+chats  = ['-1001573921800', '-1001753303143', '-1001762993891']
+
+
+LOG_CHAT = '-1539543587'
+
+ADMINS = [1341095837, 1004250581]
